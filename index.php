0ddfbf3ec39b92a43bc6c4be4f3bc6dfd009e0a6
Kulikov Bogdan <bonakulik@gmail.com>
2021-03-16 11:56:29 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require('./debugging.php');
+
+// Get login and passwords
+$login = $_GET['login'];
+$password = $_GET['password'];
+
+
+console_log('login: ' . $login);
+console_log('password: ' . $password);
+
+
