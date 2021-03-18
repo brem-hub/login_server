d148debfec50000a9f6c422bf4eaeda3f21cf755
Kulikov Bogdan <bonakulik@gmail.com>
2021-03-18 22:37:25 +0300

    Added class SQL Manager that helps with getting info from SQL db.

--- public/index.php
+++ public/index.php
@@ -5,30 +5,31 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../scripts/debugging.php';
 require __DIR__ . '/../scripts/mysql_manager.php';
+require __DIR__ . '/../scripts/global_methods.php';
 
 // DEFINES
 define('DEBUG', 'true');
 
+$sql_manager = new MySqlManager("localhost", "root", "", "workdb");
 
 $app = AppFactory::create();
 $app->setBasePath('/php_login_server');
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 
-$app->get('/login', function (Request $request, Response $response){
+$app->get('/login', function (Request $request, Response $response) {
 
     $args = $request->getQueryParams();
     $login = $args['login'];
     $password = $args['password'];
 
-    if ($login == null || $password == null){
+    if (!check_params($login, $password)) {
         $response->getBody()->write(json_encode(
-            array(
-                'status' => 0)
+                array(
+                    'status' => 0)
             )
         );
         return $response;
@@ -42,7 +43,7 @@ $app->get('/login', function (Request $request, Response $response){
     return $response;
 });
 
-$app->get('/register', function (Request  $request, Response $response){
+$app->get('/register', function (Request $request, Response $response) {
 
     $args = $request->getQueryParams();
     $login = $args['login'];
@@ -50,51 +51,40 @@ $app->get('/register', function (Request  $request, Response $response){
     $email = $args['email'];
     $user_type = $args['type'];
 
-    if (DEBUG){
-        console_log('login '. $login);console_log('pas '. $password);console_log('email '. $email);console_log('type '. $user_type);
+    if (DEBUG) {
+        console_log('login ' . $login);
+        console_log('pas ' . $password);
+        console_log('email ' . $email);
+        console_log('type ' . $user_type);
     }
 
-    if ($login == null || $password == null || $email == null || $user_type == null){
-        $response->getBody()->write(json_encode(
-            array(
-                'status' => 0,
-            )
-        ));
-        return $response;
+    if (!check_params($login, $password, $email, $user_type)) {
+        return add_status($response, 0);
     }
 
-    $response->getBody()->write(json_encode(
-        array(
-            'status' => 1
-        )
-    ));
-    return $response;
+    return add_status($response, 1);
 });
 
-/*$app->get('/add_task', function (Request $request, Response $response){
-    $args = $request->getQueryParams();
-});*/
 
-$app->post('/add_task', function (Request $request, Response $response){
+$app->post('/create_task', function (Request $request, Response $response) use($sql_manager) {
     $params = (array)$request->getParsedBody();
 
-    $contractor_login = $params['contractor_login'];
+    $contractor_login = $params['login'];
     $task_info = $params['task_info'];
 
-    if ($contractor_login == null || $task_info == null)
-        //TODO: maybe add status: 0
-        return $response;
+    if (!check_params($contractor_login, $task_info))
+        return add_status($response, 0);
 
-    $sql_link = try_mysql_connection();
-    if ($sql_link == false)
-        //TODO: maybe add status: 0
-        return $response;
+    $user = $sql_manager->get_user($contractor_login);
 
+    if ($user == false){
+        print "Could not get user from DB";
+        return add_status($response, 0);
+    }
 
-    //$sql_request = 'INSERT INTO users ()'
+    $response->getBody()->write($user['login']);
     return $response;
 });
-
 
 
 $app->run();
--- scripts/mysql_manager.php
+++ scripts/mysql_manager.php
@@ -1,12 +1,80 @@
 <?php
 
-function try_mysql_connection() {
-    $connection_status = mysqli_connect("localhost", "root", "");
-    if ($connection_status == false){
+
+class MySqlManager{
+    private $link;
+
+    function __construct($hostname, $username, $pass, $dbname){
+        $this->link = mysqli_connect($hostname, $username, $pass, $dbname);
+        if ($this->link == false) {
+            print("Could not open connection " . mysqli_connect_error());
+        } else {
+            print("Connection was established\n");
+        }
+    }
+
+    function get_user($username){
+
+        if ($this->link == false){
+            print "Connection was not opened";
+            return false;
+        }
+        $query = 'SELECT * from users WHERE login = ?';
+
+        $stmt = mysqli_stmt_init($this->link);
+
+        if(!mysqli_stmt_prepare($stmt, $query))
+        {
+            print "Failed to prepare statement\n";
+            return false;
+        }
+
+        mysqli_stmt_bind_param($stmt, 's', $username);
+        mysqli_stmt_execute($stmt);
+
+        $results = mysqli_stmt_get_result($stmt);
+
+        if (mysqli_num_rows($results) == 0) {
+            return false;
+        }
+        // check for number of rows
+        return mysqli_fetch_array($results);
+    }
+}
+
+function try_mysql_connection()
+{
+    $connection_status = mysqli_connect("localhost", "root", "", "workdb");
+    if ($connection_status == false) {
         print("Could not open connection " . mysqli_connect_error());
-    }else{
+    } else {
         print("Connection was established");
     }
     return $connection_status;
 }
 
+
+function get_user(mysqli $link, $user_name)
+{
+    $query = 'SELECT * from users WHERE login = ?';
+
+
+    $stmt = mysqli_stmt_init($link);
+
+    if(!mysqli_stmt_prepare($stmt, $query))
+    {
+        print "Failed to prepare statement\n";
+        return false;
+    }
+
+    mysqli_stmt_bind_param($stmt, 's', $user_name);
+    mysqli_stmt_execute($stmt);
+
+    $results = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($results) == 0) {
+        return false;
+    }
+    // check for number of rows
+    return mysqli_fetch_array($results);
+}
